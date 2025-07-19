f825c2176277c7ef50376cc473867171d7c62d10
Balu Reddy <balu@Mac.lan>
2025-07-19 19:46:01 +0100

    doc sites integration

---
+++ docs/sidebars.js
@@ -0,0 +1,20 @@
+module.exports = {
+  tutorialSidebar: [
+    'intro',
+    'getting-started',
+    'agents',
+    'integrations',
+    'studio',
+    'marketplace',
+    'governance',
+    'developer-guide',
+    {
+      type: 'category',
+      label: 'Tutorials',
+      items: [
+        'tutorials/invoice-automation',
+        'tutorials/smart-factory-agent'
+      ],
+    },
+  ],
+};
